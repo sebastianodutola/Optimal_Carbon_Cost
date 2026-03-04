ac7f28fa1ad761ae854a228aac1b60b7b633e000
sebastianodutola <sebastianodutola@me.com>
2026-03-04 10:40:33 +0000

    changed ingest file to allow for pulling of multiple days

--- ingest.py
+++ ingest.py
@@ -1,5 +1,5 @@
 import httpx
-from datetime import datetime, date, timezone, time
+from datetime import datetime, date, timezone, time, timedelta
 from pathlib import Path
 import pandas as pd
 import pyarrow.parquet as pq
@@ -7,24 +7,30 @@ import pyarrow as pa
 
 DATAPATH = Path(__file__).parent.joinpath("intensity.parquet")
 BASE_URL = "https://api.carbonintensity.org.uk"
-FW48 = "/intensity/{start}/fw48h"
+FW24 = "/intensity/{date}/fw24h"
 
 now = datetime.now()
 
 
-def fetch_next(dt=now):
-    start = dt.isoformat(timespec="minutes") + "Z"
+def fetch_next(start=now, days=2):
     print(f"Fetching forcast data from {start}...")
-    url = f"{BASE_URL}{FW48.format(start=start)}"
-    response = httpx.get(url)
-    response.raise_for_status()
-    return response
+    dfs = []
+    for day in range(days):
+        date = start + timedelta(days=day)
+        date = date.isoformat(timespec="minutes") + "Z"
+        url = f"{BASE_URL}{FW24.format(date=date)}"
+        response = httpx.get(url)
+        response.raise_for_status()
+        dfs.append(pd.DataFrame(response.json()["data"]))
+    df = pd.concat(dfs)
+    df.drop_duplicates(subset=["from"], inplace=True)
+    return df
 
 
 if __name__ == "__main__":
     d = date(2026, 1, 1)
-    response = fetch_next(dt=datetime.combine(date=d, time=time(0)))
-    df = pd.DataFrame(response.json()["data"])
+    days = 30
+    df = fetch_next(start=datetime.combine(date=d, time=time(0)), days=days)
     table = pa.Table.from_pandas(df)
     print(f"Writing to {DATAPATH}...")
     pq = pq.write_table(table, DATAPATH)
